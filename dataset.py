9582b0a323fcd3ce1d9adc09e37affb9e6832f63
Joe Saad <joe.saad24@hotmail.com>
2024-11-12 00:31:21 +0200

    Refactor data splitting logic into a separate function

--- dataset.py
+++ dataset.py
@@ -3,15 +3,17 @@ import shutil
 
 IMG_PATH = "brain_tumor_dataset/"
 
-# Split the data into training, validation, and testing sets
-for CLASS in os.listdir(IMG_PATH):
-    if not CLASS.startswith("."):
-        IMG_NUM = len(os.listdir(IMG_PATH + CLASS))
-        for n, FILE_NAME in enumerate(os.listdir(IMG_PATH + CLASS)):
-            img = IMG_PATH + CLASS + "/" + FILE_NAME
-            if n < 5:
-                shutil.copy(img, "data/TEST/" + CLASS.upper() + "/" + FILE_NAME)
-            elif n < 0.8 * IMG_NUM:
-                shutil.copy(img, "data/TRAIN/" + CLASS.upper() + "/" + FILE_NAME)
-            else:
-                shutil.copy(img, "data/VAL/" + CLASS.upper() + "/" + FILE_NAME)
+
+def split_data():
+    # Split the data into training, validation, and testing sets
+    for CLASS in os.listdir(IMG_PATH):
+        if not CLASS.startswith("."):
+            IMG_NUM = len(os.listdir(IMG_PATH + CLASS))
+            for n, FILE_NAME in enumerate(os.listdir(IMG_PATH + CLASS)):
+                img = IMG_PATH + CLASS + "/" + FILE_NAME
+                if n < 5:
+                    shutil.copy(img, "data/TEST/" + CLASS.upper() + "/" + FILE_NAME)
+                elif n < 0.8 * IMG_NUM:
+                    shutil.copy(img, "data/TRAIN/" + CLASS.upper() + "/" + FILE_NAME)
+                else:
+                    shutil.copy(img, "data/VAL/" + CLASS.upper() + "/" + FILE_NAME)
